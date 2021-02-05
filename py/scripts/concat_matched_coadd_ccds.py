a262c317a447cf74ed01da35123ef1d0b07c3a31
Aaron Meisner <ameisner@fas.harvard.edu>
2021-02-04 22:55:10 -0800

    median summary table

--- py/scripts/concat_matched_coadd_ccds.py
+++ py/scripts/concat_matched_coadd_ccds.py
@@ -6,6 +6,50 @@ import os
 
 basedir = '/global/cfs/cdirs/desi/users/ameisner/GFA/reduced/v0022_matched'
 
+def cube_index_median(tab, extra_cuts=False):
+
+    # make quality cuts
+
+    bad = (tab['MAX_Q1'] == 0) | (tab['MAX_Q2'] == 0) | (tab['MAX_Q3'] == 0) | (tab['MAX_Q4'] == 0) | (tab['NPIX_BAD_TOTAL'] >= 10)
+
+    is_coadd = np.max(tab['CUBE_INDEX']) == -1
+
+    contrast_thresh = 1.85 if is_coadd else 2.0
+
+    if extra_cuts:
+        bad = np.logical_or(bad, tab['CONTRAST'] < contrast_thresh)
+        bad = np.logical_or(bad, tab['N_SOURCES_FOR_PSF'] < 3)
+
+
+    print(len(tab))
+    tab = tab[np.logical_not(bad)]
+    print(len(tab))
+    
+    _id = np.array([str(tab[i]['EXPID']).zfill(8) + str(tab[i]['CUBE_INDEX']).zfill(5) for i in range(len(tab))])
+
+    ids_u = np.unique(_id)
+
+    print(len(ids_u), ' !!!!!!')
+
+    rows = []
+    for id_u in ids_u:
+        _tab = tab[_id == id_u]
+
+        row = Table()
+
+        for colname in ['EXPID', 'CUBE_INDEX', 'NIGHT', 'EXPTIME', 'FNAME_RAW', 'SKYRA', 'SKYDEC', 'PROGRAM', 'MOON_ILLUMINATION', 'MOON_ZD_DEG', 'MOON_SEP_DEG', 'KTERM']:
+            row[colname] = [_tab[0][colname]]
+
+        for colname in ['MJD', 'FWHM_ASEC', 'TRANSPARENCY', 'SKY_MAG_AB', 'FIBER_FRACFLUX', 'FIBER_FRACFLUX_ELG', 'AIRMASS', 'RADPROF_FWHM_ASEC']:
+            row[colname] = [np.nanmedian(_tab[colname])]
+
+        rows.append(row)
+
+    print(len(rows))
+    result = vstack(rows)
+
+    return result
+
 def _nights_list(night_min, night_max, basedir=basedir):
 
     dirs = glob.glob(basedir + '/????????')
@@ -57,5 +101,8 @@ def _concat_many_nights(night_min='20201214', night_max='99999999'):
     sind = np.argsort(result['EXPID'] + 0.1*result['PETAL_LOC'])
 
     result = result[sind]
+
+    med = cube_index_median(result)
+    _med = cube_index_median(result, extra_cuts=True)
     
     return result
